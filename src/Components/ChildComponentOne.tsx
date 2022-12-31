8b6bd3e8862f221a6aaf231b98256a4441ab64b7
chrstowr <ctorresj08@gmail.com>
2022-12-31 14:24:22 -0600

    Add prop example

--- src/Components/ChildComponentOne.tsx
+++ src/Components/ChildComponentOne.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
-function ChildComponentOne() {
+function ChildComponentOne(props: { PropOne: string, Number: number }) {
 
-    return (<div>Child Component One</div>)
+    return (
+        <div>
+            Child Component One
+            <div style={{ marginLeft: 15 }}>
+                {props.PropOne}
+            </div>
+            <div style={{ marginLeft: 15 }}>
+                {props.Number}
+            </div>
+            
+        </div>
+    );
 }
 
 export default ChildComponentOne;
