793b5a8ef087819e958219540dc7e1da1db248f1
chrstowr <ctorresj08@gmail.com>
2022-12-31 14:24:13 -0600

    Add JSX and JSX interpolation example

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,17 @@ import './App.css';
 import ParentComponent from './Components/ParentComponent'
 
 //App entry point, similar to index.html
-
 function App() {
+
+  // JSX is a syntax extention of functions
+  function HelloWorld() { return (<h1>Hello world!</h1>) }
+  // You can also render components with curly braces 
+  const FizzBuzz = <h2>FizzBuzz</h2>;
+
   return (
     <div>
+      <HelloWorld />
+      {FizzBuzz}
       <ParentComponent />
     </div>
   );
