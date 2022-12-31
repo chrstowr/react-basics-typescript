74c4e28c623321e5b51ed90d4c7af67546962bad
chrstowr <ctorresj08@gmail.com>
2022-12-31 13:16:32 -0600

    Remove boilerplate and add starter components

---
+++ src/Components/ChildComponentOne.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function ChildComponentOne() {
+
+    return (<div>Child Component One</div>)
+}
+
+export default ChildComponentOne;
---
+++ src/Components/ChildComponentTwo.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function ChildComponentTwo() {
+
+    return (<div>Child Component Two</div>)
+}
+
+export default ChildComponentTwo;
